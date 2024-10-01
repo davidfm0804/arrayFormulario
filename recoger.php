54b4db9db6af9598afad0de2c7c015e4662c4b57
David <davidfarinamorena.guadalupe@alumnado.fundacionloyola.net>
2024-10-01 20:22:38 +0200

    Post

--- recoger.php
+++ recoger.php
@@ -8,31 +8,31 @@
 <body>
     <h1>Datos Recogidos</h1>
     <?php
-       if (!empty($_GET['nombre'])){
-        echo "<p>Nombre: ".$_GET['nombre']."</p>";
+       if (!empty($_POST['nombre'])){
+        echo "<p>Nombre: ".$_POST['nombre']."</p>";
        }
-       if (!empty($_GET['apellidos'])){
-        echo "<p>Apellidos: ".$_GET['apellidos']."</p>";
+       if (!empty($_POST['apellidos'])){
+        echo "<p>Apellidos: ".$_POST['apellidos']."</p>";
         }
-        if (!empty($_GET['genero'])){
-        echo "<p>Genero: ".$_GET['genero']."</p>";
+        if (!empty($_POST['genero'])){
+        echo "<p>Genero: ".$_POST['genero']."</p>";
         }
-        if (!empty($_GET['edad'])){
-        echo "<p>Edad: ".$_GET['edad']."</p>";
+        if (!empty($_POST['edad'])){
+        echo "<p>Edad: ".$_POST['edad']."</p>";
         }
-        if(isset($GET_['clase'])){
-            echo "<p>Clase: ".$_GET['clase']."</p>";
+        if(isset($_POST['clase'])){
+            echo "<p>Clase: ".$_POST['clase']."</p>";
         }
-        if (isset($_GET['deportes'])){
-        foreach($_GET['deportes'] as $indice =>$valor){
+        if (isset($_POST['deportes'])){
+        foreach($_POST['deportes'] as $indice =>$valor){
             echo "<p>Deporte: ".$valor."</p>";
         }}
-        //echo "<p>".$_GET['deportes']."</p>";
-        if (!empty($_GET['fecha_nacimiento'])){
-        echo "<p>Fecha de nacimiento: ".$_GET['fecha_nacimiento']."</p>";
+        //echo "<p>".$_POST['deportes']."</p>";
+        if (!empty($_POST['fecha_nacimiento'])){
+        echo "<p>Fecha de nacimiento: ".$_POST['fecha_nacimiento']."</p>";
         }
-        if (isset($_GET['terminos'])){
-         echo "<p>Terminos: ".$_GET['terminos']."</p>";
+        if (isset($_POST['terminos'])){
+         echo "<p>Terminos: ".$_POST['terminos']."</p>";
         }
     ?>
 </body>
