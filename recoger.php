7134b1a270ae2dc7d70b5fa4cce294bc4290777e
David <davidfarinamorena.guadalupe@alumnado.fundacionloyola.net>
2024-10-01 10:36:45 +0200

    Actualizado

--- recoger.php
+++ recoger.php
@@ -8,16 +8,19 @@
 <body>
     <h1>Datos Recogidos</h1>
     <?php
-       if (empty($_GET['nombre'])){
+       if (!empty($_GET['nombre'])){
         echo "<p>".$_GET['nombre']."</p>";
        }
         echo "<p>".$_GET['apellidos']."</p>";
         echo "<p>".$_GET['genero']."</p>";
         echo "<p>".$_GET['edad']."</p>";
-        echo "<p>".$_GET['clase']."</p>";
+        if(isset($GET_['clase'])){
+            echo "<p>".$_GET['clase']."</p>";
+        }
+        if (isset($_GET['deportes'])){
         foreach($_GET['deportes'] as $indice =>$valor){
             echo "<p>".$valor."</p>";
-        }
+        }}
         //echo "<p>".$_GET['deportes']."</p>";
         echo "<p>".$_GET['fecha_nacimiento']."</p>";
         if (isset($_GET['terminos'])){
