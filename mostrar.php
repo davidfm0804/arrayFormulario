3b73b1ab7bc0497821ad831f3be32a15ad336ace
David <davidfarinamorena.guadalupe@alumnado.fundacionloyola.net>
2024-10-01 10:07:02 +0200

    first commit

---
+++ mostrar.php
@@ -0,0 +1,6 @@
+<?php
+    print_r($_GET);
+    echo "<br/>";
+    echo "<br/>";
+    var_dump($_GET);
+?>
---
+++ recoger.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recoger</title>
+</head>
+<body>
+    <h1>Datos Recogidos</h1>
+    <?php
+       if (empty($_GET['nombre'])){
+        echo "<p>".$_GET['nombre']."</p>";
+       }
+        echo "<p>".$_GET['apellidos']."</p>";
+        echo "<p>".$_GET['genero']."</p>";
+        echo "<p>".$_GET['edad']."</p>";
+        echo "<p>".$_GET['clase']."</p>";
+        foreach($_GET['deportes'] as $indice =>$valor){
+            echo "<p>".$valor."</p>";
+        }
+        //echo "<p>".$_GET['deportes']."</p>";
+        echo "<p>".$_GET['fecha_nacimiento']."</p>";
+        if (isset($_GET['terminos'])){
+         echo "<p>".$_GET['terminos']."</p>";
+        }
+    ?>
+</body>
+</html>
